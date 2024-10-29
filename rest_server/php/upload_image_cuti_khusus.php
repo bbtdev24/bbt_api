df4d3c631ce9c351c03940c27a71aa4f68cdccad
bbtdev24 <acc.dev.bbt@gmail.com>
2024-10-29 22:55:31 +0700

    change path all upload image

--- rest_server/php/upload_image_cuti_khusus.php
+++ rest_server/php/upload_image_cuti_khusus.php
@@ -1,13 +1,43 @@
 <?php
+// $no_pengajuan_tahunan = $_POST['no_pengajuan_khusus'];
+// $image = base64_decode($_POST['foto']);
+// $nama = $no_pengajuan_tahunan;
+
+// $targer_dir = "C:/xampp/htdocs/project/ess-api-android-bt/rest_server/image/upload_cuti_khusus/" . $nama . ".jpeg";
+
+// if (file_put_contents($targer_dir, $image)) {
+//     echo json_encode(array('response' => 'Success'));
+// } else {
+//     echo json_encode(array("response" => "Image not uploaded"));
+// }
+
 $no_pengajuan_tahunan = $_POST['no_pengajuan_khusus'];
 $image = base64_decode($_POST['foto']);
-
 $nama = $no_pengajuan_tahunan;
 
-$targer_dir = "C:/xampp/htdocs/project/ess-api-android-bt/rest_server/image/upload_cuti_khusus/" . $nama . ".jpeg";
+// Windows
+// $targer_dir = "C:/xampp/htdocs/project/ess-api-android-bt/rest_server/image/upload_cuti_khusus/" . $nama . ".jpeg";
+
+// Linux
+$targer_dir = "/var/www/html/bbt_api/rest_server/image/upload_cuti_khusus/" . $nama . ".jpeg";
+
+// Tambahin pengecekan apakah direktori ada dan bisa di-write
+if (!is_writable(dirname($targer_dir))) {
+    echo json_encode(array('response' => 'Directory is not writable: ' . dirname($targer_dir)));
+    exit;
+}
+
+// Tambahin pengecekan apakah base64 image valid
+if ($image === false) {
+    echo json_encode(array('response' => 'Invalid base64 image data'));
+    exit;
+}
 
 if (file_put_contents($targer_dir, $image)) {
     echo json_encode(array('response' => 'Success'));
 } else {
-    echo json_encode(array("response" => "Image not uploaded"));
+    // Tambahin error detail ketika gagal menyimpan gambar
+    $error = error_get_last();
+    echo json_encode(array("response" => "Image not uploaded", "error" => $error['message']));
 }
+
--- rest_server/php/upload_image_vaksin.php
+++ rest_server/php/upload_image_vaksin.php
@@ -1,14 +1,44 @@
 <?php
+// $no_pengajuan_tahunan = $_POST['nomor'];
+// $image = base64_decode($_POST['foto']);
+// $nama = $no_pengajuan_tahunan;
+
+// // $targer_dir = "//192.168.4.182/htdocs/eis/uploads/vaksin/".$nama.".jpeg";
+// $targer_dir = "C:/xampp/htdocs/project/ess-api-android-bt/rest_server/image/upload_vaksin/" . $nama . ".jpeg";
+
+// if (file_put_contents($targer_dir, $image)) {
+//     echo json_encode(array('response' => 'Success'));
+// } else {
+//     echo json_encode(array("response" => "Image not uploaded"));
+// }
+
 $no_pengajuan_tahunan = $_POST['nomor'];
 $image = base64_decode($_POST['foto']);
-
 $nama = $no_pengajuan_tahunan;
 
-// $targer_dir = "//192.168.4.182/htdocs/eis/uploads/vaksin/".$nama.".jpeg";
-$targer_dir = "C:/xampp/htdocs/project/ess-api-android-bt/rest_server/image/upload_vaksin/" . $nama . ".jpeg";
+// Windows
+// $targer_dir = "C:/xampp/htdocs/project/ess-api-android-bt/rest_server/image/upload_vaksin/" . $nama . ".jpeg";
+
+// Linux
+$targer_dir = "/var/www/html/bbt_api/rest_server/image/upload_vaksin/" . $nama . ".jpeg";
+
+// Tambahin pengecekan apakah direktori ada dan bisa di-write
+if (!is_writable(dirname($targer_dir))) {
+    echo json_encode(array('response' => 'Directory is not writable: ' . dirname($targer_dir)));
+    exit;
+}
+
+// Tambahin pengecekan apakah base64 image valid
+if ($image === false) {
+    echo json_encode(array('response' => 'Invalid base64 image data'));
+    exit;
+}
 
 if (file_put_contents($targer_dir, $image)) {
     echo json_encode(array('response' => 'Success'));
 } else {
-    echo json_encode(array("response" => "Image not uploaded"));
+    // Tambahin error detail ketika gagal menyimpan gambar
+    $error = error_get_last();
+    echo json_encode(array("response" => "Image not uploaded", "error" => $error['message']));
 }
+
