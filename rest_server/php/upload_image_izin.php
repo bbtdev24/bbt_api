6bd0f9c510717b989386dacc2381bd72cb490ee1
pandupradana <pandupradana50@gmail.com>
2024-10-29 15:18:26 +0700

    edit file_upload

--- rest_server/php/upload_image_izin.php
+++ rest_server/php/upload_image_izin.php
@@ -8,7 +8,7 @@ $nama = $no_pengajuan_full_day;
 //$targer_dir = "C:/xampp/htdocs/project/ess-api-android-bt/rest_server/image/upload_izin/" . $nama . ".jpeg";
 
 //linux
-$targer_dir = "/var/www/html/bbt_api/restserver/image/upload_izin/".$nama_folder."/".$nama.".jpeg";
+$targer_dir = "/var/www/html/bbt_api/rest_server/image/upload_izin/".$nama_folder."/".$nama.".jpeg";
 
 if (file_put_contents($targer_dir, $image)) {
     echo json_encode(array('response' => 'Success'));
